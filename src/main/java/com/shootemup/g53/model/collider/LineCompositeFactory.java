201a0a6b1138945490553e9e154e953bbbf92105
Nuno Alves <nyansword@gmail.com>
2021-05-06 23:41:34 +0100

    add: from circle composite and tests

--- src/main/java/com/shootemup/g53/model/collider/LineCompositeFactory.java
+++ src/main/java/com/shootemup/g53/model/collider/LineCompositeFactory.java
@@ -26,12 +26,35 @@ public class LineCompositeFactory {
         return new LineCompositeCollider(element, colliderHashMap);
     }
 
-    LineCompositeCollider createFromIsoscelesTriangle(Element element, Position center, int height) {
+    LineCompositeCollider createFromIsoscelesTriangle(Element element, Position top, int height) {
         HashMap<Integer, LineCollider> colliderHashMap = new HashMap<>();
 
         for(int y = 0; y <= height; y++) {
             colliderHashMap.put(
-                    y+center.getY(), new LineCollider(element, new Position(center.getX()-y, y+ center.getY()), 2*(y))
+                    y+top.getY(), new LineCollider(element, new Position(top.getX()-y, y+ top.getY()), 2*(y))
+            );
+        }
+
+        return new LineCompositeCollider(element, colliderHashMap);
+    }
+
+    LineCompositeCollider createFromCircle(Element element, Position center, int radius) {
+        HashMap<Integer, LineCollider> colliderHashMap = new HashMap<>();
+
+        if(radius == 0) return  new LineCompositeCollider(element, colliderHashMap);
+
+        colliderHashMap.put(
+                center.getY(), new LineCollider(element, center.getLeft(radius), 2*radius)
+        );
+
+        for(int r = 1; r < radius+1; r++) {
+            int currentRadius = radius - r;
+
+            colliderHashMap.put(
+                    center.getY()+r, new LineCollider(element, center.getLeft(currentRadius).getUp(r), 2*(currentRadius))
+            );
+            colliderHashMap.put(
+                    center.getY()-r, new LineCollider(element, center.getLeft(currentRadius).getDown(r), 2*(currentRadius))
             );
         }
 
--- src/test/java/com/shootemup/g53/model/collider/LineCompositeFactoryTest.java
+++ src/test/java/com/shootemup/g53/model/collider/LineCompositeFactoryTest.java
@@ -102,9 +102,65 @@ class LineCompositeFactoryTest {
         for(int y = 0; y <= 5; y++) {
             Assertions.assertEquals(y+1, heightsList.get(y));
             Assertions.assertEquals(y+1, colliderList.get(y).topLeft.getY());
-            Assertions.assertEquals(heightsList.get(y), colliderList.get(y).topLeft.getY()); // tautology after the past two but wtv
+            Assertions.assertEquals(heightsList.get(y), colliderList.get(y).topLeft.getY());
             Assertions.assertEquals(2-y, colliderList.get(y).topLeft.getX());
             Assertions.assertEquals(y*2, colliderList.get(y).width);
         }
+    }
+
+    @Test
+    void fromCircle() {
+        LineCompositeFactory factory = new LineCompositeFactory();
+
+        LineCompositeCollider compositeCollider = factory.createFromCircle(
+                element2, new Position(2, 1), 5
+        );
+
+        BoundingBox bb = compositeCollider.getRealBoundingBox();
+        Assertions.assertEquals(new Position(2-5,1-5+2), bb.getTopLeft());
+        Assertions.assertEquals(5*2, bb.getWidth());
+        Assertions.assertEquals(5*2, bb.getHeight());
+
+        Collection<LineCollider> colliders = compositeCollider.getColliderHashMap().values();
+        Collection<Integer> heights = compositeCollider.getColliderHashMap().keySet();
+        List<LineCollider> colliderList = new ArrayList<>(colliders);
+        List<Integer> heightsList = new ArrayList<>(heights);
+
+        colliderList.sort(Comparator.comparingInt(o -> o.topLeft.getY()));
+        Collections.sort(heightsList);
+
+        Assertions.assertEquals(1, heightsList.get(5));
+        Assertions.assertEquals(1, colliderList.get(5).topLeft.getY());
+        Assertions.assertEquals(heightsList.get(5), colliderList.get(5).topLeft.getY());
+        Assertions.assertEquals(2-5, colliderList.get(5).topLeft.getX());
+        Assertions.assertEquals(5*2, colliderList.get(5).width);
+
+        for(int y = 1; y <= 5; y++) {
+            int currentRadius = 5 - y;
+
+            Assertions.assertEquals(1+y, heightsList.get(y+5));
+            Assertions.assertEquals(1+y, colliderList.get(y+5).topLeft.getY());
+            Assertions.assertEquals(heightsList.get(y+5), colliderList.get(y+5).topLeft.getY());
+            Assertions.assertEquals(2-currentRadius, colliderList.get(y+5).topLeft.getX());
+            Assertions.assertEquals(currentRadius*2, colliderList.get(y+5).width);
+
+            Assertions.assertEquals(1-y, heightsList.get(5-y));
+            Assertions.assertEquals(1-y, colliderList.get(5-y).topLeft.getY());
+            Assertions.assertEquals(heightsList.get(5-y), colliderList.get(5-y).topLeft.getY());
+            Assertions.assertEquals(2-currentRadius, colliderList.get(5-y).topLeft.getX());
+            Assertions.assertEquals(currentRadius*2, colliderList.get(5-y).width);
+        }
+
+        LineCompositeCollider compositeCollider2 = factory.createFromCircle(
+                element2, new Position(2, 1), 0
+        );
+
+        Collection<LineCollider> colliders2 = compositeCollider2.getColliderHashMap().values();
+        Collection<Integer> heights2 = compositeCollider2.getColliderHashMap().keySet();
+        List<LineCollider> colliderList2 = new ArrayList<>(colliders2);
+        List<Integer> heightsList2 = new ArrayList<>(heights2);
+
+        Assertions.assertEquals(0, colliderList2.size());
+        Assertions.assertEquals(0, heightsList2.size());
     }
 }
